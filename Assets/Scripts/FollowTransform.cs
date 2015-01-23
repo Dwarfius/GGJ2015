f40200e4de6e36d2f65d4221121d1512853b6cd5
Dwarfius <youman6@gmail.com>
2015-01-23 21:13:32 +0000

    PlayerController commit

---
+++ Assets/Scripts/FollowTransform.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTransform : MonoBehaviour 
+{
+    public Transform target;
+
+    void Update () 
+    {
+        Vector3 pos = target.position;
+        pos.z = -10;
+        transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    public Vector2 speed = Vector2.one;
+
+    float spriteHeight;
+
+    void Start()
+    {
+        spriteHeight = (renderer as SpriteRenderer).sprite.bounds.size.y / transform.localScale.y;
+    }
+
+    void Update()
+    {
+        bool jumping = Input.GetButton("Jump");
+        if (jumping && Physics2D.Raycast(transform.position, -Vector2.up, spriteHeight))
+            rigidbody2D.AddForce(new Vector2(0, speed.y), ForceMode2D.Impulse);
+
+        float dx = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(dx * speed.x * Time.deltaTime, 0, 0));
+    }
+}
