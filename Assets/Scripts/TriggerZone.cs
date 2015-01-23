2ac4f5e1955a3f68faec3f283d8ac8b84d89423b
Dwarfius <youman6@gmail.com>
2015-01-23 21:44:08 +0000

    Triggers and controlelr updated

---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class TriggerZone : MonoBehaviour 
+{
+    public UnityEvent EnteredTrigger;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player" && EnteredTrigger != null)
+            EnteredTrigger.Invoke();
+    }
+}
