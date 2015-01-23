e6c0852271b7c3cd5476ad133e66505c908f1398
ricardobarros95 <ricardobarros95@gmail.com>
2015-01-23 21:51:01 +0000

    gamedata & text writeout

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameData : MonoBehaviour {
+    static GameData instance;
+    public static GameData Instance
+    {
+        get 
+        {
+            if(instance == null)
+            {
+                GameObject obj = new GameObject();
+                DontDestroyOnLoad(obj);
+                instance = obj.AddComponent<GameData>();
+            }
+            return instance;
+        }
+    }
+
+    public int[] allAnswers = new int[10];
+}
