2d69e0a9716bd70c6438fe05db940860554abec0
Dwarfius <youman6@gmail.com>
2015-01-23 22:09:20 +0000

    fixed jumping bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,24 @@ public class PlayerController : MonoBehaviour
     public Vector2 speed = Vector2.one;
 
     float spriteHeight;
+    bool jumping;
 
     void Start()
     {
-        spriteHeight = (renderer as SpriteRenderer).sprite.bounds.size.y / transform.localScale.y;
+        spriteHeight = (collider2D as BoxCollider2D).bounds.size.y / transform.localScale.y;
     }
 
     void Update()
     {
-        bool jumping = Input.GetButton("Jump");
-        if (jumping && Physics2D.Raycast(transform.position, -Vector2.up, spriteHeight))
-            rigidbody2D.AddForce(new Vector2(0, speed.y), ForceMode2D.Impulse);
+        jumping = Input.GetButton("Jump");
 
         float dx = Input.GetAxis("Horizontal");
         transform.Translate(new Vector3(dx * speed.x * Time.deltaTime, 0, 0));
+    }
+
+    void FixedUpdate()
+    {
+        if (jumping && Physics2D.Raycast(transform.position, -Vector2.up, spriteHeight / 1.6f))
+            rigidbody2D.AddForce(new Vector2(0, speed.y), ForceMode2D.Impulse);
     }
 }
