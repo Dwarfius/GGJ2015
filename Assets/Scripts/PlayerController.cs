e8fd67ad9ccdd44c3bab04e1690b840ea04c19ef
Dwarfius <youman6@gmail.com>
2015-01-24 12:49:06 +0000

    added melee and ranged attacks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
     public Vector2 speed = Vector2.one;
+    public GameObject projectilePrefab;
 
     float spriteHeight;
     bool jumping;
+    Animator animator;
+    bool facing = true;
+    Collider2D arm;
 
     void Start()
     {
+        arm = transform.GetChild(0).collider2D;
+        animator = GetComponent<Animator>();
         spriteHeight = collider2D.bounds.size.y / transform.localScale.y;
-        Debug.Log(GameData.Instance);
+        Debug.Log(GameData.Instance); //forcing it to spawn to have a menu
     }
 
     void Update()
@@ -19,7 +25,21 @@ public class PlayerController : MonoBehaviour
         jumping = Input.GetButton("Jump");
 
         float dx = Input.GetAxis("Horizontal");
+        facing = dx > 0 ? true : dx < 0 ? false : facing;
         transform.Translate(new Vector3(dx * speed.x * Time.deltaTime, 0, 0));
+        Vector3 v = transform.localScale;
+        v.x = facing ? Mathf.Abs(v.x) : -Mathf.Abs(v.x);
+        transform.localScale = v;
+
+        if (Input.GetButtonDown("Attack"))
+            Attack();
+        if (Input.GetButtonDown("Fire"))
+            animator.SetBool("preppingShot", true);
+        if (Input.GetButtonUp("Fire"))
+        {
+            animator.SetBool("preppingShot", false);
+            Fire();
+        }
     }
 
     void FixedUpdate()
@@ -28,5 +48,33 @@ public class PlayerController : MonoBehaviour
             rigidbody2D.AddForce(new Vector2(0, speed.y), ForceMode2D.Impulse);
     }
 
-    
+    void Attack()
+    {
+        animator.SetBool("attacking", true);
+        arm.enabled = true;
+        StartCoroutine(armCd());
+    }
+
+    void Fire()
+    {
+        Quaternion angle = Quaternion.Euler(0, 0, facing ? 30 : 150);
+        Vector3 n = Vector3.right;
+        GameObject projectile = (GameObject)Instantiate(projectilePrefab, transform.position + (facing ? n : -n) / 6, angle);
+        projectile.GetComponent<Arrow>().SetTargetTag("Enemy");
+        projectile.rigidbody2D.velocity = 5 * projectile.transform.right;
+    }
+
+    //used to recieve triggers from the sword arm
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Enemy")
+            Destroy(col.gameObject);
+    }
+
+    IEnumerator armCd()
+    {
+        yield return new WaitForSeconds(1.083f);
+        arm.enabled = false;
+        animator.SetBool("attacking", false);
+    }
 }
