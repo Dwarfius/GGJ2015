536dceae7bfab3ca31e3ad7353518dc59da2fd0d
ricardobarros95 <ricardobarros95@gmail.com>
2015-01-24 16:14:48 +0000

    MORE TUTORIAL STUFF!

---
+++ Assets/Scripts/HiddenDoor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiddenDoor : Interactible {
+
+    public override void Interact(PlayerController player)
+    {
+        Application.LoadLevel("TestScene");
+    }
+}
