aa6970eeaa42320d0f15fad29a49d0bf7d6405e3
H <hamishzeb91@gmail.com>
2015-01-25 12:14:43 +0100

    soundtrack

---
+++ Assets/Scripts/AudioPlugin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPlugin
+{
+    public static void PlayClip(string name, float delay = 0)
+    {
+        AudioClip clip = Resources.Load<AudioClip>(name);
+        GameObject obj = new GameObject("AudioPlayer");
+        AudioSource source = obj.AddComponent<AudioSource>();
+        source.clip = clip;
+		if(delay == 0)
+        	source.Play();
+		else
+			source.PlayDelayed(delay);
+        GameObject.Destroy(obj, clip.length + delay);
+    }
+}
