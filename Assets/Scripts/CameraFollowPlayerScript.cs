f984f30e096faa645cc6b4fb8808b234601c6918
H <hamishzeb91@gmail.com>
2015-01-25 04:45:12 +0100

    Tutorial done
    finished, needs testing once started a normal game.

---
+++ Assets/Scripts/CameraFollowPlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayerScript : MonoBehaviour 
+{
+	public GameObject target;
+	public Vector3 offset;
+
+	private void Update()
+	{
+		if (!target)
+			return;
+	
+		Vector3 wantedPos = (target.transform.position + offset);
+		wantedPos.y = offset.y;
+		transform.position = Vector3.Lerp(transform.position, wantedPos, Time.deltaTime * 5);
+	}
+}
