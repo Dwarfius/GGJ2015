0b2ca9e4a32c532e358e07c38fb57bce6db1f9db
ricardobarros95 <ricardobarros95@gmail.com>
2015-01-25 12:06:37 +0000

    donkey levels

---
+++ Assets/Scripts/Missions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missions : MonoBehaviour {
+
+	// Use this for initialization
+	public void EndLevel () {
+        GameData d = GameData.Instance;
+        switch(d.allAnswers[2])
+        {
+            case 1:
+                Application.LoadLevel("VillageFood");
+                break;
+            case 2:
+                Application.LoadLevel("VillageFood");
+                break;
+            case 3:
+                GetComponent<DonkeyAnomaly>().enabled = true;
+                enabled = false;
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
